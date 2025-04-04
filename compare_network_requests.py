976b8a35e980d469a0d915613c82e73338529054
Brenda Reyes Ayala <48692385+reyesayala@users.noreply.github.com>
2025-04-04 16:58:54 -0600

    Update compare_network_requests.py
    program no longer removes AIT-specific requests
    try/except statement for opening files
    outputs results of analysis to csv file

--- compare_network_requests.py
+++ compare_network_requests.py
@@ -10,7 +10,7 @@ def find_different_requests(request_pair):
     Parameters
     ----------
     request_pair : tuple
-        Tuple containing a single current network request info in the first index and 
+        Tuple containing a single current network request info in the first index and
         a single archive network request info in the second index.
 
     """
@@ -19,7 +19,7 @@ def find_different_requests(request_pair):
     a_request = request_pair[1]
     c_status = c_request["status_code"]
     a_status = a_request["status_code"]
-	
+
     # we do not consider redirects with status 302 to be errors
     if(c_status!= a_status) and (a_status!="302") and (c_status!="302"):
         #print("Difference found!")
@@ -48,132 +48,180 @@ def check_archive_specific_url(dict_obj):
     else:
         return True
 
+
+
 def open_with_csv(index_file_name, curr_requests_path, arch_requests_path, csv_out_name, do_print):
     """Parses both index files line by line and writes the urls and file names to the output file.
-   
+
     Parameters
     ----------
     index_file_name : str
         The CSV file that maps the current network requests to their archived counterparts.
     curr_csv_path : str
-    	The directory containing the CSV files with the current websites' network requests.
+        The directory containing the CSV files with the current websites' network requests.
     arch_out_path : str
-    	The directory containing the CSV files with the archived websites' network requests.
+        The directory containing the CSV files with the archived websites' network requests.
     csv_out_name : str
         The CSV file to write the file with the combined requests.
     do_print : bool
-    	Whether or not to print the results to stdout.
-		
+        Whether or not to print the results to stdout.
+
     """
-	
-    # Read the index file 
+    with open(csv_out_name, 'w') as csv_file_out:
+        csv_writer = csv.writer(csv_file_out, delimiter=',', quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(["current_url", "current_filename", "archive_url", "archive_filename", "num_current_requests","num_archive_requests", "num_requests_in_common", "num_requests_with_errors", "ic"])
+    
+
+    # Read the index file
     index_lst = []
     index_file = open(index_file_name, 'r')
     index_reader = csv.reader(index_file, delimiter=',')
-	
+
     # Skip the headers
     next(index_reader, None)
+    
 
     for column in index_reader:
         index_lst.append(column)
 
     index_file.close()
     print("Index file has been read")
-	
+
     for request_file in index_lst:
         c_url = request_file[0]
         a_url = request_file[1]
         c_file_name = request_file[2]
         a_file_name = request_file[3]
-		
-        # Read the csv files as dicts
-        with open(arch_requests_path + "/" + a_file_name) as a_input:
-            a_input_file = csv.DictReader(a_input)
-            arch_requests_data = [row for row in a_input_file]
 
-            # Close file
-            a_input.close()
-	
-        with open(curr_requests_path + "/" + c_file_name) as c_input:
-            c_input_file = csv.DictReader(c_input)
-            curr_requests_data = [row for row in c_input_file]
-	    
-            # Close file
-            c_input.close()
-		
-        num_differences = 0
-        missing_requests = 0
-	
-        print("Comparing ", c_file_name, " to ", a_file_name)
+        print("-----------------------------------------------------")
+        print("Current url: ", c_url)
+        print("Archive url: ", a_url)
 
-        # Filter out archive specific URLS
-        arch_requests_data = list(filter(check_archive_specific_url, arch_requests_data))
+        try:
+            # Read the csv files as dicts
+            with open(arch_requests_path + "/" + a_file_name) as a_input:
+                a_input_file = csv.DictReader(a_input)
+                arch_requests_data = [row for row in a_input_file]
 
-        for a_row in arch_requests_data:
-            a_url = a_row["url"]
-	    #print("Archived url: ", a_url)
-	    #find the matching element in list of current network requests, from here:
-	    #https://stackoverflow.com/questions/8653516/python-list-of-dictionaries-search
-	    #a_row = list(filter(lambda a_request: (c_url in a_request["url"]), arch_requests_data))
-			
-            c_row = list(filter(lambda c_request: (fuzz.partial_ratio(a_url.lower(),c_request["url"].lower())>90), curr_requests_data))
-	    
-            if c_row: 
-	    	#print("Match found!")
-		#print("Current: ", c_url)
-		#print("Archived: ", a_row[0]["url"])
-                c_url = c_row[0]["url"]
-                request_pair = (c_row[0], a_row)
-		
-                # Checks if status code are different between two requests
-                if find_different_requests(request_pair):
-                    num_differences = num_differences + 1
+                # Close file
+                a_input.close()
+                print("There are ",len(arch_requests_data)," requests in archived website")
 
+            with open(curr_requests_path + "/" + c_file_name) as c_input:
+                c_input_file = csv.DictReader(c_input)
+                curr_requests_data = [row for row in c_input_file]
+
+                # Close file
+                c_input.close()
+                print("There are ",len(curr_requests_data)," requests in current website")
+
+
+
+            num_errors = 0
+            missing_requests = 0
+
+            print("=============================================")
+            print("Comparing ", c_file_name, " to ", a_file_name)
+
+            # Filter out archive specific URLS -- UNDO
+            #arch_requests_data = list(filter(check_archive_specific_url, arch_requests_data))
+            matched_requests = []
+
+            for a_row in arch_requests_data:
+                a_url = a_row["url"]
+            #print("Archived url: ", a_url)
+            #find the matching element in list of current network requests, from here:
+            #https://stackoverflow.com/questions/8653516/python-list-of-dictionaries-search
+            #a_row = list(filter(lambda a_request: (c_url in a_request["url"]), arch_requests_data))
+
+                c_row = list(filter(lambda c_request: (fuzz.partial_ratio(a_url.lower(),c_request["url"].lower())>90), curr_requests_data))
+
+                if c_row:
+                    print("Match found!")
+                    print("Current: ", c_url)
+                    print("Archived: ", a_url)
+                    c_url = c_row[0]["url"]
+                    request_pair = (c_row[0], a_row)
+                    matched_requests.append(request_pair)
+
+                    # Checks if status code are different between two requests
+                    if find_different_requests(request_pair):
+                        num_errors = num_errors + 1
+
+                else:
+                    print("Network request not found: ", a_url)
+                    missing_requests = missing_requests + 1
+
+            # Number of network requests that have a different status code in archived site vs. current site
+            if num_errors > 0:
+                print("There are {} requests that have produced errors in the current and archived websites".format(num_errors))
             else:
-                print("Archived request not found: ", a_url)
-                missing_requests = missing_requests + 1
-	
-        # Number of differences between current and archived website
-        if num_differences > 0:
-            print("There are {} differences between the current and archived websites".format(num_differences))
-        else:
-            print("No differences found")
-			
-        # 
-        if missing_requests > 0:
-            print("There are {} requests in the archived site that are not present in the current site".format(missing_requests))
-        else:
-            print("No missing requests found")
-			
-        int_correspondence = (len(arch_requests_data)-missing_requests-num_differences)/len(arch_requests_data)
-        print("Interactional correspondence: ", int_correspondence)
-		    	
+                print("No differences found")
+
+            #
+            if missing_requests > 0:
+                print("There are {} requests in the archived site that are not present in the current site".format(missing_requests))
+            else:
+                print("No missing requests found")
+
+            #what the hell was I thinking?
+            #int_correspondence = (len(arch_requests_data)-missing_requests-num_differences)/len(arch_requests_data)
+            #int_correspondence_current = (len(arch_requests_data)-missing_requests-num_differences)/len(curr_requests_data)
+            print("There are ", len(matched_requests)," requests both in the current and the archived version")
+
+            num_requests_in_common = len(matched_requests)
+            if num_requests_in_common!=0:
+                requests_with_no_errors = num_requests_in_common - num_errors
+                print("Requests with no errors: ", requests_with_no_errors)
+                print("No. of requests in common: ", num_requests_in_common)
+                ic = requests_with_no_errors/num_requests_in_common
+                print("Interactional correspondence: ", ic)
+                print("===================================================")
+            else:
+                print("Interactional correspondence: ", 0)
+                print("===================================================")
+
+
+
+
+
+
+
+        except FileNotFoundError:
+            print("File Not found: ", arch_requests_path + "/" + a_file_name)
+            
+        with open(csv_out_name, 'a') as csv_file_out:
+            csv_writer = csv.writer(csv_file_out, delimiter=',', quoting=csv.QUOTE_ALL)
+            csv_writer.writerow([c_url, c_file_name, a_url, a_file_name, len(curr_requests_data), len(arch_requests_data),
+                                 num_requests_in_common, num_errors, ic])
+            
+            
 def parse_args():
     """Parses the command line arguments
 
     Returns
     -------
     index_file_name : str
-    	The CSV file that maps the current network requests to their archived counterparts.
+        The CSV file that maps the current network requests to their archived counterparts.
     curr_requests_path : str
-    	The directory containing the CSV files with the current websites' network requests.	
+        The directory containing the CSV files with the current websites' network requests.
     arch_requests_path : str
-    	The directory containing the CSV files with the archived websites' network requests.
+        The directory containing the CSV files with the archived websites' network requests.
     csv_out_name : str
-    	The CSV file to write the file with the combined requests.
+        The CSV file to write the file with the combined requests.
     do_print : bool
-    	Whether or not to print the results to stdout.
+        Whether or not to print the results to stdout.
 
     """
 
     parser = argparse.ArgumentParser()
 
     # initializing every line switch
-    parser.add_argument("--index_file",	type=str, help="The CSV	file that maps the current network requests to their archived counterparts")
+    parser.add_argument("--index_file", type=str, help="The CSV file that maps the current network requests to their archived counterparts")
     parser.add_argument("--curr_requests", type=str, help="The directory containing the CSV files with the current websites' network requests")
     parser.add_argument("--arch_requests", type=str, help="The directory containing the CSV files with the archived websites' network requests")
     parser.add_argument("--out", type=str, help="The CSV file to write file with the combined requests")
-    parser.add_argument("--print", action='store_true', help="(optional) Include to print the network requests, default doesn't print")
+    parser.add_argument("--print_output", action='store_true', help="(optional) Include to print the network requests, default doesn't print")
 
     args = parser.parse_args()
 
@@ -202,12 +250,13 @@ def parse_args():
 
     index_file_name = args.index_file
     csv_out_name = args.out
-    do_print = args.print
+    do_print = args.print_output
 
-    return index_file_name, curr_requests_path,	arch_requests_path, do_print, csv_out_name
+    return index_file_name, curr_requests_path, arch_requests_path, do_print, csv_out_name
 
 def main():
     index_file_name, curr_requests_path, arch_requests_path, do_print, csv_out_name = parse_args()
     open_with_csv(index_file_name, curr_requests_path, arch_requests_path, csv_out_name, do_print)
 
 main()
+
